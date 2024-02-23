7ca8156bb32b0fd99d46414a9d123eb52618dc9f
Mehdi Salimi <salimimehdi@yahoo.com>
2024-02-23 13:40:28 +0100

    now

---
+++ WhiteLagoon.Web/ViewModels/RegisterVM.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [DisplayName("Phone(Mobile) Number")]
+        public string PhoneNumber { get; set; }
+        public string? RedirectUrl { get; set; }
+
+    }
+}
