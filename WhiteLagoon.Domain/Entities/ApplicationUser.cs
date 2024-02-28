62b312cefd40e89e8f044fd6b09e9534216a639c
Mehdi Salimi <salimimehdi@yahoo.com>
2024-02-28 02:31:37 +0100

    now

--- WhiteLagoon.Domain/Entities/ApplicationUser.cs
+++ WhiteLagoon.Domain/Entities/ApplicationUser.cs
@@ -11,7 +11,7 @@ namespace WhiteLagoon.Domain.Entities
     public class ApplicationUser : IdentityUser
     {
         public string Name { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt   { get; set; }
 
     }
 
